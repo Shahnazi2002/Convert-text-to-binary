fc11ab5dc6ef014de67b1dd698578f5ccd940ab8
Mohammad SN <shahnazi2002@gmail.com>
2022-01-12 01:25:36 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 def toBinary(string):
     result = ''
     for character in string:
-        if character == ' ':
-            result = result + ' '
-        elif character == '.':
-            result = result + '.'
-        else:
-            result = result + bin(ord(character))[2:].zfill(8) + ' '
+        result = result + bin(ord(character))[2:].zfill(8) + ' '
     return result.strip()
 
 
